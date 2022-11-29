e08d1ea18cd43c4ded93b9760099c4941754570e
arc-015 <chavanavinash015@gmail.com>
2022-11-29 21:54:26 +0530

    Added : Weocome to the Address Book Program with Connector JDBC

---
+++ src/main/java/com/bridgelabz/JdbcAddressBook.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class JdbcAddressBook {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Address Book Program with the MySQL DataBase wit the Connector JDBC");
+
+    }
+}
